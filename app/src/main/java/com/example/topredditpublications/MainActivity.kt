576eb72abab8e39ccf471e7586a145b2e1efc27f
antony <antony@pop-os.localdomain>
2020-01-23 12:55:41 +0200

    added saving image to gallery

--- app/src/main/java/com/example/topredditpublications/MainActivity.kt
+++ app/src/main/java/com/example/topredditpublications/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.topredditpublications
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
@@ -14,24 +26,45 @@ import com.example.topredditpublications.dataSource.RedditPost
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_dialog.view.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+import java.util.jar.Manifest
 import android.os.Bundle as Bundle1
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-
-
-
+    private val RECORD_REQUEST_CODE = 101
+//    var myDrawable: Drawable = ivImageURL.drawable
+//    val bitmap: Bitmap = (myDrawable as BitmapDrawable).bitmap
+//    val bit: Bitmap =myDrawable.toBitmap()
 
      private val adapter = PostAdapter {
          val mDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
          val dialog = AlertDialog.Builder(this).setView(mDialog)
          val alert = dialog.show()
 
+
+
          alert.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
          Picasso.get().load(it.url).into(mDialog.ivImageURL)
          mDialog.bnSave.setOnClickListener {
-             Toast.makeText(this, "save" , Toast.LENGTH_SHORT).show()
-             alert.dismiss()
+             val permission = ContextCompat.checkSelfPermission(this,
+                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+             if (permission != PackageManager.PERMISSION_GRANTED) {
+                 makeRequest()
+             }else {
+                 val myDrawable = mDialog.ivImageURL.drawable
+                 val bitmap: Bitmap = (myDrawable as BitmapDrawable).bitmap
+
+                 val uri: Uri = saveImageToExternalStorage(bitmap, "saveImage")
+                 toast("Image saved successful.$uri")
+                 alert.dismiss()
+             }
          }
 
          mDialog.bnCancel.setOnClickListener {
@@ -41,13 +74,52 @@ class MainActivity : AppCompatActivity() {
 
      }
 
+    private fun saveImageToExternalStorage(bitmap: Bitmap, save: String): Uri {
+
+        val savedImageURL = MediaStore.Images.Media.insertImage(
+            contentResolver,
+            bitmap,
+            save,
+            "Image of $save"
+        )
+        return Uri.parse(savedImageURL)
+    }
+
     override fun onCreate(savedInstanceState: Bundle1?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupPermissions()
         initList()
     }
 
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            RECORD_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            RECORD_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                }
+            }
+        }
+    }
 
     private fun initList() {
 
@@ -80,23 +152,9 @@ class MainActivity : AppCompatActivity() {
         return LivePagedListBuilder<String,RedditPost>(dataSourceFactory,config!!)
     }
 
-//    fun bt(view: View) {
-//
-//        val mDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
-//        val dialog = AlertDialog.Builder(this).setView(mDialog)
-//        val a = dialog.show()
-//        a.tV.text = "dgdfg"
-//
-//        mDialog.buttonOk.setOnClickListener {
-//
-//            Toast.makeText(this, "ok" , Toast.LENGTH_SHORT).show()
-//
-//
-//            a.dismiss()
-//
-//        }
-//    }
-
 
 }
 
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
